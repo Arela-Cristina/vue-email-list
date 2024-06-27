d290b8f6f9d95ac5dcbaaa3c3c63692e8488a00b
Arela-Cristina <instagrandine@gmail.com>
2024-06-27 16:20:31 +0200

    refact arrayMails data sctructure

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-        //aggiungo un array de e-mails
-      arrayMails: [
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        ''
-      ],
+    //dati della app
+    data() {
+        return {
+            //aggiungo un array de e-mails
+            arrayMails: [
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+                { email: '' },
+            ],
+        }
+    },
+    //comportamenti della app
+    methods: {
+
+    },
+    // ancora non ho capito a caosa serve :v 
+    mounted() {
+        console.log('ancora non ho capito a  cosa servi');
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(mails => {
+                const array = mails.data.arrayMails;
+                console.log('hey', array);
+            });
+
     }
-  }
 }).mount('#app')
