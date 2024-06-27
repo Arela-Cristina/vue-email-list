7abef51a1d6362eabe0b315a3949c28227b54670
Arela-Cristina <instagrandine@gmail.com>
2024-06-27 16:37:33 +0200

    verifica

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const { createApp } = Vue
 createApp({
     //dati della app
     data() {
+
         return {
             //aggiungo un array de e-mails
             arrayMails: [
-                { email: '' },
-                { email: '' },
-                { email: '' },
-                { email: '' },
+                { email: '1' },
+                { email: '2' },
+                { email: '3' },
+                { email: '4' },
                 { email: '' },
                 { email: '' },
                 { email: '' },
@@ -17,20 +18,26 @@ createApp({
                 { email: '' },
                 { email: '' },
             ],
+
         }
     },
     //comportamenti della app
     methods: {
 
     },
-    // ancora non ho capito a caosa serve :v 
+    //ancora non ho capito a caosa serve :v 
     mounted() {
         console.log('ancora non ho capito a  cosa servi');
+        
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(mails => {
-                const array = mails.data.arrayMails;
+                const array = mails.data.response;
                 console.log('hey', array);
+                this.emailarrayMails.email = array;
+
             });
 
     }
+
 }).mount('#app')
+
