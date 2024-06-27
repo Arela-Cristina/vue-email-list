97af0ca8c9daba41e54fe449ad78d88697708900
Arela-Cristina <instagrandine@gmail.com>
2024-06-27 17:12:35 +0200

    Bonus

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ createApp({
         return {
             //aggiungo un array de e-mails
             arrayMails: [
-                
+
             ],
 
         }
@@ -16,18 +16,19 @@ createApp({
     methods: {
 
     },
-    //ancora non ho capito a caosa serve :v 
+    //ancora non ho capito a cosa serve :v 
     mounted() {
         console.log('ancora non ho capito a  cosa servi');
+        //facciamo un ciclo for, che ripete axios 10 volte, cosi ch stampiamo 10 mail.
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(mails => {
+                    const array = mails.data.response;
+                    console.log('hey', array);
+                    this.arrayMails.push(array);
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(mails => {
-                const array = mails.data.response;
-                console.log('hey', array);
-                this.arrayMails.push(array);
-
-            });
-
+                });
+        }
     }
 
 }).mount('#app')
